cb18ac93aa0c889dc80fd6c7b5b3bc19a2df314e
Jessica Vo <vojessi410@gmail.com>
2022-02-16 13:36:59 -0500

    shapes moving down screen

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,5 +1,84 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector("grid");
+  let squares = Array.from(document.querySelector(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBin = document.querySelector("#start-button");
+  const width = 10;
 
+  // The Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
 
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
 
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  let current position = 4
+  
+  // randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][0]
+
+  // draw the first rotation in the first tetromino
+  function draw() {
+      current.forEach(index => {
+          squares[currentPosition + index].classList.add('tetromino')
+      })
+
+  }
+
+  // undraw the Tetromino
+  function undraw() {
+      current.forEach(index => {
+          squares[currentPosition + index].classList.remove('tetromino')
+      })
+  }
+
+  // make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000)
+
+  // move down function
+  function moveDown() {
+      undraw()
+      currentPosition += width
+      draw()
+  }
+
+  
 });
