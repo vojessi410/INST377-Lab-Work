62fe9d5ba2d50d0c2c8301be56d8e73eb773a94a
Jessica Vo <vojessi410@gmail.com>
2022-02-25 03:00:00 -0500

    finish script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -18,28 +18,32 @@ document.
 
 function updateSlidePosition() {
     for (let slide of slides) {
-        console.log(slide);
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
     }
+
+    slides[slidePosition].classList.add('carousel_item--visible');
 }
 
 function moveToNextSlide() {
-    updateSlidePosition();
 
     if (slidePosition === totalSlides - 1) {
         slidePosition = 0;
     } else {
         slidePosition++;
     }
+    updateSlidePosition();
 };
 
 function moveToPrevSlide() {
-    updateSlidePosition();
 
     if (slidePosition === 0) {
-        slidePosition = 0;
+        slidePosition = totalSlides - 1;
     } else {
         slidePosition--;
     }
+    updateSlidePosition();
+
 };
 
 
