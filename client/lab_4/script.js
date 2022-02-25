e821c1545423831aeab7a3cc076b04469a456067
Jessica Vo <vojessi410@gmail.com>
2022-02-25 03:09:42 -0500

    update css

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,49 +1,44 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('carousel_item');
+const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
 console.log(slides);
 
-document.
-    document.querySelector('carousel_button--next')
-    .addEventListener("click", function() {
-        moveToNextSlide();
-    });
+document
+  .document.querySelector('#carousel_button--next')
+  .addEventListener('click', () => {
+    moveToNextSlide();
+  });
 
-document.
-    document.querySelector('carousel_button--prev')
-    .addEventListener("click", function() {
-        moveToPrevSlide();
-    });
+document
+  .document.querySelector('#carousel_button--prev')
+  .addEventListener('click', () => {
+    moveToPrevSlide();
+  });
 
 function updateSlidePosition() {
-    for (let slide of slides) {
-        slide.classList.remove('carousel_item--visible');
-        slide.classList.add('carousel_item--hidden');
-    }
+  for (const slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
 
-    slides[slidePosition].classList.add('carousel_item--visible');
+  slides[slidePosition].classList.add('carousel_item--visible');
 }
 
 function moveToNextSlide() {
-
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    updateSlidePosition();
-};
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
+}
 
 function moveToPrevSlide() {
-
-    if (slidePosition === 0) {
-        slidePosition = totalSlides - 1;
-    } else {
-        slidePosition--;
-    }
-    updateSlidePosition();
-
-};
-
-
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
+}
