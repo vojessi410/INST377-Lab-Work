926f229a6d385cce32f1cc749eecd2629b3b99a7
Jessica Vo <vojessi410@gmail.com>
2022-02-25 20:36:33 -0500

    moved around script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -3,21 +3,6 @@ const slides = document.getElementsByClassName('carousel_item');
 // const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
-document
-  .getElementById('carousel_button--next')
-  // document.querySelector('#carousel_button--next')
-  .addEventListener('click', () => {
-    // eslint-disable-next-line no-use-before-define
-    moveToNextSlide();
-  });
-
-document
-  .getElementById('#carousel_button--prev')
-  .addEventListener('click', () => {
-    // eslint-disable-next-line no-use-before-define
-    moveToPrevSlide();
-  });
-
 function updateSlidePosition() {
   // eslint-disable-next-line no-restricted-syntax
   for (const slide of slides) {
@@ -47,3 +32,18 @@ function moveToPrevSlide() {
   }
   updateSlidePosition();
 }
+
+document
+  .getElementById('carousel_button--next')
+  // document.querySelector('#carousel_button--next')
+  .addEventListener('click', () => {
+    // eslint-disable-next-line no-use-before-define
+    moveToNextSlide();
+  });
+
+document
+  .getElementById('#carousel_button--prev')
+  .addEventListener('click', () => {
+    // eslint-disable-next-line no-use-before-define
+    moveToPrevSlide();
+  });
