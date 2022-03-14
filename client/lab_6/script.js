f5c6a1cac164ac0950782c2ea154d4612d6bfb65
Jessica Vo <vojessi410@gmail.com>
2022-03-13 23:54:54 -0400

    added html create list

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -4,7 +4,7 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (newMax - newMin + 1) + min);
 }
 
-function dataHandler(dataArray) {
+function restoArrayMake(dataArray) {
   console.log('fired dataHandler');
   console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
@@ -15,6 +15,10 @@ function dataHandler(dataArray) {
   
   console.log(listItems);
   return listItems;
+}
+
+function createHtmlList(collection){
+  console.log(collection);
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
@@ -34,7 +38,8 @@ async function mainEvent() { // the async keyword means we can make API requests
       console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      dataHandler(arrayFromJson.data);
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
   }
 }
