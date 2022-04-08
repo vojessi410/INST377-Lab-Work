bc0a91cbfc81418212346196cd4cd6db8ca96816
Jessica Vo <vojessi410@gmail.com>
2022-04-08 02:14:41 -0400

    added index and style sheet to lab 8

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -36,7 +36,7 @@ async function mainEvent() {
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit_button');
   const resto = document.querySelector('#restaurant_name');
-  const zipcode = document.querySelector('#zipcode');
+  const type = document.querySelector('#food_type');
   submit.style.display = 'none';
 
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
@@ -52,15 +52,33 @@ async function mainEvent() {
       if (currentArray.length < 1) {
         return;
       }
+    })
+
+    let currentArray1 = [];
+    type.addEventListener('input', async (event1) => {
+        console.log(event1.target.value);
+        if (currentArray1.length < 1) {
+          return;
+        }
+      })
 
       const selectResto = currentArray.filter((item) => {
         const lowerName = item.name.toLowerCase();
         const lowerValue = event.target.value.toLowerCase();
         return lowerName.includes(lowerValue);
       });
+
+      const selectType = currentArray1.filter((item) => {
+        const lowerName1 = item.name.toLowerCase();
+        const lowerValue1 = event1.target.value.toLowerCase();
+        return lowerName1.includes(lowerValue1);
+      });
+
       console.log(selectResto);
       createHtmlList(selectResto);
-    });
+      console.log(selectType);
+      createHtmlList(selectType);
+    };
 
     form.addEventListener('submit', async (submitEvent) => {
       // async has to be declared all the way to get an await
@@ -71,6 +89,10 @@ async function mainEvent() {
       currentArray = restoArrayMake(arrayFromJson.data);
       console.log(currentArray);
       createHtmlList(currentArray);
+
+      currentArray1 = restoArrayMake(arrayFromJson.data);
+      console.log(currentArray1);
+      createHtmlList(currentArray1);
     });
   }
 }
